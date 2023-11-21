42dfedd4b2df727cc21683c0940e85c784b834d2
najatul6 <mdnajatulislam.develop@gmail.com>
2023-11-22 03:43:26 +0600

    Create Add item form using react hook form

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function run() {
 
     app.post('/jwt', async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' });
       res.send({ token });
     })
 
