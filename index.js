a367381a1c50a9584ab8c5a24f620a6dbfd83e6e
najatul6 <mdnajatulislam.develop@gmail.com>
2023-11-22 10:51:52 +0600

    Delete menu item with VerifyAdmin middleware

--- index.js
+++ index.js
@@ -142,6 +142,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/menu/:id',verifyToken,verifyAdmin, async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId (id)};
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // -------------------
     // Reviews Collection
     // -------------------
